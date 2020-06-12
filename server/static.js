2dc1ca9c94bdde9c6e49d1244eb68542857e8d44
Thomaz T. O. Toyama <thomazot@gmail.com>
2020-06-12 20:46:17 -0300

    Ajuste no lightbox e na categoria

--- server/static.js
+++ server/static.js
@@ -1,19 +1,25 @@
-const config = require('./config');
+const config = require('./config')
 module.exports = [
-  {
-    route: '/media/interface/0/'+ config.theme +'/css',
-    dir: './css'
-  },
-  {
-    route: '/media/interface/0/'+ config.theme +'/js',
-    dir: './js'
-  },
-  {
-    route: '/backstop',
-    dir: './backstop_data'
-  },
-  {
-    route: '/media/files',
-    dir: ['./components/src', './components/dist', './img', './svg']
-  }
+    {
+        route:
+            '/media/amoptimization/default/media/interface/0/' +
+            config.theme +
+            '/css',
+        dir: './css',
+    },
+    {
+        route:
+            '/media/amoptimization/default/media/interface/0/' +
+            config.theme +
+            '/js',
+        dir: './js',
+    },
+    {
+        route: '/backstop',
+        dir: './backstop_data',
+    },
+    {
+        route: '/media/files',
+        dir: ['./components/src', './components/dist', './img', './svg'],
+    },
 ]
